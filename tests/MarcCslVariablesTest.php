1bfa97223ebeaac9885e2f117973a6f9ff82c9b6
dolf <rudolfbyker@gmail.com>
2020-08-18 17:51:01 +0200

    Extract helper function for testGetAll.

--- tests/MarcCslVariablesTest.php
+++ tests/MarcCslVariablesTest.php
@@ -55,7 +55,8 @@ XML
    * Test the getAll method.
    */
   public function testGetAll() {
-    $xml = <<<XML
+    $this->assertGetAll(
+      <<<XML
 <?xml version="1.0" encoding="UTF-8"?>
 <record>
   <datafield tag="245">
@@ -78,20 +79,36 @@ XML
     <subfield code="b">Oxford University Press</subfield>
   </datafield>
 </record>
-XML;
+XML,
+      [
+        'title' => "'Let the Dead Bury Their Dead' (Matt. 8:22/Luke 9:60) : Jesus and the Halakhah",
+        'container-title' => 'The Journal of Theological Studies',
+        'page' => '553-581',
+        'page-first' => '553',
+        'volume' => '49',
+        'issue' => '2',
+        'url' => 'https://www.jstor.org/stable/23968765',
+        'author' => [['family' => 'Markus Bockmuehl']],
+        'issued' => ['raw' => 'October 1998'],
+        'publisher' => 'Oxford University Press',
+      ],
+      "TODO"
+    );
+  }
+
+  /**
+   * Helper function for testGetAll.
+   *
+   * @param string $xml
+   *   The XML data to parse.
+   * @param array $expected
+   *   The expected result.
+   * @param string $message
+   *   The assertion message.
+   */
+  private function assertGetAll(string $xml, array $expected, string $message): void {
     $marcCsl = new MarcCslVariables(Record::fromString($xml));
-    $this->assertEquals([
-      'title' => '\'Let the Dead Bury Their Dead\' (Matt. 8:22/Luke 9:60) : Jesus and the Halakhah',
-      'container-title' => 'The Journal of Theological Studies',
-      'page' => '553-581',
-      'page-first' => '553',
-      'volume' => '49',
-      'issue' => '2',
-      'url' => 'https://www.jstor.org/stable/23968765',
-      'author' => [['family' => 'Markus Bockmuehl']],
-      'issued' => ['raw' => 'October 1998'],
-      'publisher' => 'Oxford University Press',
-    ], $marcCsl->getAll());
+    $this->assertEquals($expected, $marcCsl->getAll(), $message);
   }
 
 }
